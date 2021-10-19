78c2f7055128c61cbfd1a14dc6e088af1ce932e9
Nahid hossain <nahidhossain124@gmail.com>
2021-10-19 21:21:15 +0600

    first commit

---
+++ React/my-web/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render("hello", document.getElementById("root"));
+ReactDOM.render("my name is eblo", document.getElementById("root"));
